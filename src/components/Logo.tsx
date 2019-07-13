6c580c1a4eebca918db6bd312f616f39cd9007a0
Shu Uesugi <shu@chibicode.com>
2019-07-13 14:35:45 -0700

    Add svgr

---
+++ src/components/Logo.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+
+const SvgShuLogoBoxed = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 400 400" {...props}>
+    <title>{'shu-logo-boxed'}</title>
+    <desc>{'Created with Sketch.'}</desc>
+    <g
+      id="shu-logo-boxed"
+      stroke="none"
+      strokeWidth={1}
+      fill="none"
+      fillRule="evenodd"
+    >
+      <rect
+        id="Rectangle"
+        stroke="#222222"
+        strokeWidth={20}
+        x={10}
+        y={10}
+        width={380}
+        height={380}
+        rx={50}
+      />
+      <g
+        id="Sigma"
+        transform="translate(125.000000, 100.000000)"
+        stroke="#222222"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={20}
+      >
+        <polyline
+          id="Path-2"
+          transform="translate(75.000000, 100.000000) rotate(-360.000000) translate(-75.000000, -100.000000) "
+          points="150 0 0 0 150 200 0 200 75 100"
+        />
+      </g>
+    </g>
+  </svg>
+)
+
+export default SvgShuLogoBoxed
