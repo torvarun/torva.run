c86be919806411c09dcde9ac938f8982f2c0c731
Shu Uesugi <shu@chibicode.com>
2019-11-18 16:00:37 -0800

    Fix logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,17 +2,8 @@ import * as React from 'react'
 
 const SvgShuLogoBoxed = (props: React.SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 400 400" {...props}>
-    <title>{'shu-logo-boxed'}</title>
-    <desc>{'Created with Sketch.'}</desc>
-    <g
-      id="shu-logo-boxed"
-      stroke="none"
-      strokeWidth={1}
-      fill="none"
-      fillRule="evenodd"
-    >
+    <g stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
       <rect
-        id="Rectangle"
         stroke="currentColor"
         strokeWidth={20}
         x={10}
@@ -22,7 +13,6 @@ const SvgShuLogoBoxed = (props: React.SVGProps<SVGSVGElement>) => (
         rx={50}
       />
       <g
-        id="Sigma"
         transform="translate(125.000000, 100.000000)"
         stroke="currentColor"
         strokeLinecap="round"
@@ -30,7 +20,6 @@ const SvgShuLogoBoxed = (props: React.SVGProps<SVGSVGElement>) => (
         strokeWidth={20}
       >
         <polyline
-          id="Path-2"
           transform="translate(75.000000, 100.000000) rotate(-360.000000) translate(-75.000000, -100.000000) "
           points="150 0 0 0 150 200 0 200 75 100"
         />
