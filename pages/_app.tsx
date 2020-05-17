e01da161c2735a9133d3b70ae849b2fe94331f71
Shu Uesugi <shu@chibicode.com>
2020-05-16 18:20:58 -0700

    Remove unused gtag (#3)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,9 @@ import 'src/styles/iosevka.css'
 import 'src/styles/global.css'
 import { AppProps } from 'next/app'
 import reloadTwitterScript from 'src/lib/reloadTwitterScript'
-import { pageview } from 'src/lib/gtag'
 
 Router.events.on('routeChangeStart', () => {
   reloadTwitterScript()
-})
-
-Router.events.on('routeChangeComplete', (path: string) => {
-  pageview(path)
 })
 
 export default function App({ Component, pageProps }: AppProps) {
