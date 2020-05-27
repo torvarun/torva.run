d099e469c0222ffc2197516dbdb976072710937e
Shusaku Uesugi <shu@chibicode.com>
2020-05-27 16:18:40 -0700

    Add redirects

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  experimental: {
+    async redirects() {
+      return [
+        // Linked from https://www.kalzumeus.com/2014/11/07/doing-business-in-japan/
+        {
+          source: '/doing-business-in-japan{/}?',
+          permanent: true,
+          destination: 'https://jp.chibicode.com/doing-business-in-japan',
+        },
+      ]
+    },
+  },
+}
