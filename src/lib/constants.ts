c0a48cddb688b6f11b7b37b23f42ea31a372c075
torvarun <torvarun@gmail.com>
2021-01-23 23:08:49 -0500

    update constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Shu Uesugi'
-export const SITE_URL = 'https://chibicode.com'
-export const SITE_DESCRIPTION = '👞 @chibicode 🥃'
+export const SITE_TITLE = 'torvarun'
+export const SITE_URL = 'https://torva.run'
+export const SITE_DESCRIPTION = '@torvarun'
