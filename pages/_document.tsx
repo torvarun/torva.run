f15c7801f17d4f5e6dd6e8cdb5d6374726629df7
torvarun <torvarun@gmail.com>
2021-01-31 16:01:32 -0500

    remove twitter refs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,6 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script async src="https://platform.twitter.com/widgets.js" />
         </body>
       </Html>
     )
