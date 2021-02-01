d4136a37e57ef3f0fe8f8f1d7a4f82ee1106a2a7
torvarun <torvarun@gmail.com>
2021-01-31 19:22:15 -0500

    add css styles for the d3 viz

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import 'src/styles/iosevka.css'
 import 'src/styles/inter.css'
 import 'src/styles/global.css'
+import 'src/styles/data_viz.css'
 import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
