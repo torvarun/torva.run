c7cf25cf3641f3146cd34803ff9674a78d7ad840
torvarun <torvarun@gmail.com>
2021-01-31 23:48:45 -0500

    ignore typescript errors when building

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   async redirects() {
     return [
       // Linked from https://www.kalzumeus.com/2014/11/07/doing-business-in-japan/
